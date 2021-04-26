305aa6f1b2dd2c88c24a6390c240231a5e0143c0
jepore97 <jepore97@gmail.com>
2021-04-26 13:57:32 -0500

    modulo facturas con boton add

---
+++ src/Modelos/Item.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modelos;
+
+/**
+ *
+ * @author H-P
+ */
+public class Item {
+    private String producto;
+    private int cantidad;
+    private int precio;
+
+    public Item(String producto, int cantidad, int precio) {
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precio = precio;
+    }
+
+    public String getProducto() {
+        return producto;
+    }
+
+    public void setProducto(String producto) {
+        this.producto = producto;
+    }
+
+    public int getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(int cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+    
+    
+}
